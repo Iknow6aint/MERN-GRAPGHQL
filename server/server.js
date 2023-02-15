9dcc01f30b3e770a8364765d5ffdd105546456c7
iknow6aint <saintagbukor@gmail.com>
2023-02-15 12:41:03 +0100

    server init

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
-import express from "express"
+const express = require('express')
 
+require('dotenv').config();
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 
 const app = express()
 
