e7a2ebfea9516c8bb9e6bc6c3861231b56ca79ce
iknow6aint <saintagbukor@gmail.com>
2023-02-16 01:45:45 +0100

    sample data add

--- server/server.js
+++ server/server.js
@@ -1,10 +1,16 @@
 const express = require('express')
-
+const { graphqlHTTP } = require('express-graphql')
+const schema = require('./schema/schema')
 require('dotenv').config();
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === 'developement'
+}))
 
 
 app.listen(PORT, () => {
